bcce7f957be295a1a43aca92eeb74c5194c7d288
Manchineel <manchineel@infinitedesert.com>
2022-07-27 23:51:06 +0200

    feat: ✨ Handle SIGTERM/SIGINT gracefully

--- mariadb_drive_dump/__main__.py
+++ mariadb_drive_dump/__main__.py
@@ -1,10 +1,12 @@
 from pycron import has_been
 from datetime import datetime, timedelta
 from time import sleep
+from signal import signal, SIGINT, SIGTERM
 
 from utils.job import DumpJob
 from utils.config import crontab
 from utils.maintenance import time_of_last_backup, spring_clean
+from utils.utils import get_logger
 
 
 def launch_job():
@@ -12,6 +14,24 @@ def launch_job():
     job = DumpJob()
     job.thread.join()
 
+
+def on_signal(signal_number, frame):
+    del frame
+    logger = get_logger(__name__)
+    logger.info(f"Received signal {signal_number}")
+    global job
+    if job is None or job.finished:
+        logger.info("No job running, exiting immediately. Goodbye!")
+        exit(0)
+    else:
+        logger.info("Still terminating current backup job, then exiting")
+        job.thread.join()
+        logger.info("Skipping spring clean routine. Goodbye!")
+        exit(0)
+
+
+signal(SIGINT, on_signal)
+signal(SIGTERM, on_signal)
 
 job: DumpJob = None
 last_backup_time: datetime = time_of_last_backup()
--- mariadb_drive_dump/utils/gzip.py
+++ mariadb_drive_dump/utils/gzip.py
@@ -1,12 +1,15 @@
 from io import BytesIO
 from gzip import GzipFile
 
+from .utils import get_logger
+
 
 def compress_buffer(data: BytesIO) -> BytesIO:
     compressed = BytesIO()
     with GzipFile(fileobj=compressed, mode="wb") as f:
         f.write(data.getvalue())
     compressed.seek(0, 0)
+    get_logger(__name__).info("Compressed file!")
     return compressed
 
 
@@ -15,4 +18,5 @@ def decompress_buffer(data: BytesIO) -> BytesIO:
     with GzipFile(fileobj=data, mode="rb") as f:
         decompressed.write(f.read())
     decompressed.seek(0, 0)
+    get_logger(__name__).info("Decompressed file!")
     return decompressed
